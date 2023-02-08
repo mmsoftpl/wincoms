41ce5d1766d7af92e5757771fa5f828fa12b61ce
mmsoft <contact@mmsoft.pl>
2023-02-08 10:56:07 +0100

    no message

--- SyncDeviceBluetooth/BluetoothWindowsPeerToPeer.cs
+++ SyncDeviceBluetooth/BluetoothWindowsPeerToPeer.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Concurrent;
+﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,53 +8,13 @@ namespace SyncDevice.Windows.Bluetooth
 {
     public class BluetoothWindowsPeerToPeer : BluetoothWindows
     {
-        private BluetoothLeWatcher bluetoothLeWatcher;
-        private BluetoothLePublisher bluetoothLePublisher;
-
         private BluetoothWindowsClient bluetoothWindowsClient;
         private BluetoothWindowsServer bluetoothWindowsServer;
 
         public override bool IsHost => bluetoothWindowsServer?.IsHost ?? bluetoothWindowsClient?.IsHost ?? false;
         private readonly ConcurrentDictionary<ConnectionId, ISyncDevice> PeerToPeerConnections = new ConcurrentDictionary<ConnectionId, ISyncDevice>();
         public override IList<ISyncDevice> Connections => PeerToPeerConnections.Values.ToList();
-
-        #region Bluetooth LE Watcher
-        private Task ScanForServerSignatures()
-        {
-            if (bluetoothLeWatcher == null)
-            {
-                bluetoothLeWatcher = new BluetoothLeWatcher() { Logger = Logger };
-                bluetoothLeWatcher.OnError += BluetoothLeWatcher_OnError;
-                return bluetoothLeWatcher.StartAsync(SessionName, null, $"Start scanning for '{ServiceName}' server signatures");
-            }
-            return Task.CompletedTask;
-        }
-
-        private void BluetoothLeWatcher_OnError(object sender, string error)
-        {
-            RaiseOnError(error);
-            _= StopAsync("Error while watching for server signatures ");
-        }
-
-        private Task StopScanningForServerSignatures(string reason)
-        {
-            try
-            {
-                if (bluetoothLeWatcher != null)
-                {
-                    bluetoothLeWatcher.OnError -= BluetoothLeWatcher_OnError;
-                    return bluetoothLeWatcher?.StopAsync(reason);
-                }
-                return Task.CompletedTask;
-            }
-            finally
-            {
-                bluetoothLeWatcher = null;
-            }
-        }
-
-        #endregion        
-
+      
         #region Bluetooth Client
         private Task ConnectToHost()
         {
@@ -126,50 +84,7 @@ namespace SyncDevice.Windows.Bluetooth
             }
             return Task.CompletedTask;
         }
-        #endregion
-
-        #region Bluetooth LE Publisher
-
-        private Task StartPublishingSignatureAsync()
-        {
-            var clientSignature = SessionName;
-
-            if (bluetoothLePublisher == null)
-            {
-                bluetoothLePublisher = new BluetoothLePublisher() { Logger = Logger, ServiceName = ServiceName, SessionName = clientSignature };
-                bluetoothLePublisher.OnError += BluetoothLePublisher_OnError;
-                return bluetoothLePublisher.StartAsync(clientSignature, null, $"Start publishing server signature");
-            }
-            else
-            {
-                return Task.CompletedTask;
-            }
-        }
-
-        private void BluetoothLePublisher_OnError(object sender, string error)
-        {
-                RaiseOnError(error);
-                _ = StopAsync("Error while publishing server signatures ");
-        }
-
-        private Task StopPublishingSignatureAsync(string reason)
-        {
-            try
-            {
-                if (bluetoothLePublisher != null)
-                {
-                    bluetoothLePublisher.OnError -= BluetoothLePublisher_OnError;
-                    return bluetoothLePublisher?.StopAsync(reason);
-                }
-                return Task.CompletedTask;
-            }
-            finally
-            {
-                bluetoothLePublisher = null;
-            }
-        }
-
-        #endregion
+        #endregion       
 
         #region Bluetooth Server
 
@@ -213,52 +128,16 @@ namespace SyncDevice.Windows.Bluetooth
 
         #endregion  
 
-        private async Task StartServerOrHost(CancellationToken? cancellationToken, int delayMs = 2000)
-        {
-            if (delayMs <= 0)
-                throw new System.ArgumentException("Must be greater than 0", nameof(delayMs));
-
-            if (cancellationToken != null)
-            {
-                //wait two seconds before decide which mode to activate
-                await Task.Delay(delayMs, cancellationToken.Value);
-
-                if (!cancellationToken.Value.IsCancellationRequested)
-                {
-                    // No other servers signatures found, activate server mode
-                    if (bluetoothLeWatcher.Signatures.Count == 0)
-                    {
-                        await StartPublishingSignatureAsync();
-                        await StartHosting();
-                        await StopScanningForServerSignatures("Starting in server mode, no need to scan for other servers");
-                    }
-                    else
-                    {
-                        // Some server singature found, activate client mode
-                        await ConnectToHost();
-                    }
-
-                    startAsyncTokenSource = null;
-                    Status = SyncDeviceStatus.Started;
-                }
-            }
-        }
-
-        CancellationTokenSource startAsyncTokenSource = null;
-
         public async Task startAsync(string sessionName, string pin, int delayMs = 2000)
         {
             if (Status == SyncDeviceStatus.Stopped)
             {
-                startAsyncTokenSource = new CancellationTokenSource();
-
                 Connections.Clear();
                 SessionName = sessionName;
                 Pin = pin;
 
-                await ScanForServerSignatures();
-
-                await StartServerOrHost(startAsyncTokenSource?.Token, delayMs);
+                await StartHosting();
+                await ConnectToHost();
             }
         }
 
@@ -269,13 +148,9 @@ namespace SyncDevice.Windows.Bluetooth
 
         public override async Task StopAsync(string reason)
         {
-            startAsyncTokenSource?.Cancel();
-            startAsyncTokenSource = null;
             Status = SyncDeviceStatus.Stopped;
             await StopHosting(reason);
             await DisconnectFromHost(reason);
-            await StopScanningForServerSignatures(reason);
-            await StopPublishingSignatureAsync(reason);
         }
 
         public override async Task RestartAsync(string reason)
